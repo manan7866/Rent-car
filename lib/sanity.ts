b982d3787054036cc4d69d517980f053c08a7903
Amwighio <159983597+manan7866@users.noreply.github.com>
2025-01-01 21:33:58 -0800

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,1 +1,9 @@
+import { createClient } from '@sanity/client';
 
+// Create and export the Sanity client
+export const sanityClient = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // Environment variable
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // Environment variable
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // If you're using an API token
+  useCdn: process.env.NODE_ENV === 'production', // Using CDN for production builds
+});
